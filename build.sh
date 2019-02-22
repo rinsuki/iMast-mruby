c800d437c1172f617eeb51751b7f9eedcbedda47
rinsuki <428rinsuki+git@gmail.com>
2019-02-22 19:37:38 +0900

    added binary

--- build.sh
+++ build.sh
@@ -5,5 +5,7 @@ set -xe
 cd src/mruby
 ./minirake
 cd ../..
-mkdir -p dist/lib
-lipo src/mruby/build/ios{,_sim}/lib/libmruby.a -create -output dist/lib/libmruby.a
+rm -rf MRuby.framework
+mkdir -p MRuby.framework/Headers
+cp -r ./src/mruby/include/* MRuby.framework/Headers/
+lipo src/mruby/build/ios{,_sim}/lib/libmruby.a -create -output MRuby.framework/MRuby
